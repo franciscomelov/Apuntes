9fe46f28f1a6b27b0a8d44587cd3a2f4b30acbe0
franciscomelov <femva3012@hotmail.com>
2022-01-25 21:23:21 -0600

    notes

---
+++ reto_dataAcademy/4-variables.py
@@ -0,0 +1,24 @@
+# una caja en donde se pueden guardae objetos o valores
+# cada variable tiene un nombre identificador
+
+# vamos a crear una variable 
+# dar le un nommbre y guardar algo en la variable
+
+numero = 3
+# Dentro de la variabñe "numero" guardamos el numero 3
+
+numero1 = 2
+numero2 = 6
+
+numero1 + numero2
+# EL resultado sera 8
+
+total = numero1 + numero2
+# Ahora total tiene el valor de 8
+
+total - 8
+# El resultado es 0
+
+#_-_-_-_-_-_-
+# El nombre de la variable no debe iniciar con una letra minuscula
+
---
+++ reto_dataAcademy/5-tipos_de_datos.py
@@ -0,0 +1,43 @@
+# Tipos de datos primitivos
+
+# Numeros enteros
+# Numeros de punto flotante
+# Texto - string - Cadenas de caracteres
+# Booleanos
+
+#_-_-_-__-_-_-_-_-
+
+nombre = "Paco"
+print(nombre)
+# > Paco
+nombre2 = 'juan'
+print(nombre2)
+# > juan
+# Todo dentro de comillas es un texto - string
+
+# concatenando (unir texto)
+print(nombre + nombre2)
+# > Pacojuan
+
+# multiplicar texto por un numero
+print(nombre * 2)
+# > PacoPaco
+
+# concatenando mas texto(unir texto)
+print(nombre + ", " + nombre2)
+# > Paco, juan
+
+# floats
+numero_decimal = 3.4
+print(numero_decimal)
+# >3.4
+
+# Booleanos
+es_estudiante = True
+print(es_estudiante)
+# >True
+trabaja = False
+print(trabaja)
+# >False
+
+
