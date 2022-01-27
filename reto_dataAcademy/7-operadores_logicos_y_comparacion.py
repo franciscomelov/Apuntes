dbce306dcee9702d4348fb9449e84ff0c7eb7b7b
franciscomelov <femva3012@hotmail.com>
2022-01-27 04:37:39 -0600

    notes

---
+++ reto_dataAcademy/7-operadores_logicos_y_comparacion.py
@@ -0,0 +1,48 @@
+# and or not
+
+# Comparadores booleanos
+
+estudiante = True
+trabaja = False
+
+estudiante and trabaja
+# False
+
+estudiante or trabaja
+# True
+
+not estudiante
+# False
+
+not trabaja
+# True
+
+# _-_-_-_-_-_-_-
+# == < > >= <= !=
+
+# Comparadaroes aritmeticos
+
+2 == 2
+# True
+4 == 3
+# False
+
+2 != 2
+# False
+4 != 2
+# True
+
+1 < 2
+# true
+
+1 > 2
+# False
+
+4 <= 5
+# true
+4 <= 4
+# True
+
+4 >= 6
+# False
+
