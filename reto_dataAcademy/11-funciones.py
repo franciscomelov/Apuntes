31eed2c5a8a24b746514912f84c244d535313502
franciscomelov <femva3012@hotmail.com>
2022-01-31 22:21:47 -0600

    Clases

---
+++ reto_dataAcademy/11-funciones.py
@@ -0,0 +1,32 @@
+# def imprimir_mensaje():
+#     print("Estoy aprendiendo a uar funcoines: ")
+#     print("!Estoy aprendiendo a usar funcoines¡")
+
+# # Invocando funcion
+# imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje()
+
+
+
+# Usando parametros
+
+def conversacion(mensaje):
+    print("hola")
+    print("como estas")
+    print("mensaje")
+
+
+opcion = int(input("Elije una opcin (1, 2, 3): "))
+
+if opcion == 1:
+    conversacion("Elegiste la opcoin 1")
+elif opcion == 2:
+    conversacion("Elegiste la opcoin 2")
+elif opcion == 3:
+    conversacion("Elegiste la opcoin 3")
+else:
+    print("Elige la opcion correcta")
+
+
+
