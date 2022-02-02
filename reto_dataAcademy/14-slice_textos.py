54a7a945dc0c4d0cc3c8e6e09097117281b605ab
franciscomelov <femva3012@hotmail.com>
2022-02-02 13:27:56 -0600

    apuntes platzi

---
+++ reto_dataAcademy/14-slice_textos.py
@@ -0,0 +1,34 @@
+# slice = rebanada, rebanar
+
+# paco -> pa | co
+# se partio el texto paco por la mitad
+# p | aco
+# pac |o
+
+nombre = "Francisco"
+
+nombre[0]
+# F
+
+nombre[0:3]
+# fra
+
+# nombre[inicio: final -1]
+
+# nombre[:3] == nombre[0:3]
+# nombre[3:] == nombre[3: final de cadena]
+
+nombre[1:7]
+# rancis
+
+nombre[1:7:2]  #-> 2 == saltos
+# rni
+
+nombre[::]
+# Francisco
+
+nombre[::-1]
+# ocsicnarF
+
+
+
