5b8bbeae6cf92dfb959b85d8a50edee429924330
franciscomelov <femva3012@hotmail.com>
2022-02-02 18:33:58 -0600

    platzi notes

---
+++ reto_dataAcademy/16-bucles.py
@@ -0,0 +1,29 @@
+from operator import contains
+
+
+print(0)
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+
+contador = 0
+while(contador < 10):
+    print(contador)
+    # contador = contador + 1
+    contador += 1
+
+for i in range(10):
+    print(i)
+
+# Los codigos hacen exacta mente lo mismo
+
+#____-_-___-_-_-_-_-_-_-
+
+# range(n)
+# range(de 0 a n-1)
