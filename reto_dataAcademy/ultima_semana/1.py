d2b4ef054422aacb3e42ae8bdec85c57947454a2
franciscomelov <femva3012@hotmail.com>
2022-02-14 17:53:28 -0600

    notes

---
+++ reto_dataAcademy/ultima_semana/1.py
@@ -0,0 +1,10 @@
+# Media - Promedio
+# Mediana - Dato que esta en la mitad
+# Moda - Dato que mas se repite
+
+# Dia grama de frecuancia
+# sirve para ver los datos de forma visual
+
+# La media es susceptible a valores atípicos - extremos o muy altos o mouy bajos
+
+# La moda no aplica para datos numericos continuos
